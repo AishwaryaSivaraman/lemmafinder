b0626152218e47ae32aad849b6d72bdfee87aa46
Ana Brendel <anabrendel25@gmail.com>
2022-11-09 13:36:40 -0800

    Add the runtime options for the lfind parameter

---
+++ src/Opts.ml
@@ -0,0 +1,79 @@
+open Goptions
+
+(* HYPERPARAMETERS and RUN OPTIONS *)
+let enable_quickchick = ref true
+let enable_proverbot = ref true
+let synthesizer = ref "coqsynth"
+let synth_batch_size = ref 6
+let timeout = ref 12
+(* Can add any other parameters that you want the user to be able to set here. *)
+
+(* Setting up option for enabling proverbot 
+   To set to false: Unset Lfind Proverbot.
+   To set to true: Set Lfind Proverbot. *)
+let _ = 
+  let gdopt = 
+    {
+      optdepr=false;
+      optkey=["Lfind";"Proverbot"];
+      optname="Use Proverbot";
+      optread=(fun () -> !enable_proverbot);
+      optwrite=(fun b -> enable_proverbot := b)
+  }
+  in declare_bool_option gdopt
+
+(* Setting up option for enabling quickchick 
+   To set to false: Unset Lfind Quickchick.
+   To set to true: Set Lfind Quickchick. *)
+let _ = 
+  let gdopt = 
+    {
+      optdepr=false;
+      optkey=["Lfind";"Quickchick"];
+      optname="Use Quickchick";
+      optread=(fun () -> !enable_quickchick);
+      optwrite=(fun b -> enable_quickchick := b)
+    }
+  in declare_bool_option gdopt
+
+(* Setting up option for choosing the synthesizer 
+   To set: Set Lfind Synthesizer {name of synthesizer}.
+   To default: Unset Lfind Synthesizer.*)
+let _ = 
+  let gdopt = 
+    {
+      optdepr=false;
+      optkey=["Lfind";"Synthesizer"];
+      optname="Choose synthesizer";
+      optread=(fun () -> !synthesizer);
+      optwrite=(fun synth -> synthesizer := synth)
+    }
+  in declare_stringopt_option gdopt
+
+(* Setting up option for the number of terms synthesized (batch size) 
+   To set: Set Lfind Batch-Size {number of terms (k)}.
+   To default: Unset Lfind Batch-Size.*)
+let _ = 
+  let gdopt = 
+    {
+      optdepr=false;
+      optkey=["Lfind";"Batch-Size"];
+      optname="Choose batch-size";
+      optread=(fun () -> !synth_batch_size);
+      optwrite=(fun k -> synth_batch_size := k)
+    }
+  in declare_int_option gdopt
+
+(* Setting up option for the number of terms synthesized (batch size) 
+   To set: Set Lfind Timeout {timeout (t)}.
+   To default: Unset Lfind Timeout.*)
+let _ = 
+  let gdopt = 
+    {
+      optdepr=false;
+      optkey=["Lfind";"Timeout"];
+      optname="Choose timeout";
+      optread=(fun () -> !timeout);
+      optwrite=(fun t -> timeout := t)
+    }
+  in declare_int_option gdopt
